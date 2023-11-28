ff86e0f3743e94778f8d3dc0e0686163957e6a16
reaovyd <zackbellzokobell@gmail.com>
2023-11-28 13:58:51 -0500

    refactor: 💡 check output

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ fn main() {
                     }
                 } else if let Ok(hdr) = DnsHeader::try_from(&buf[0..12]) {
                     println!("{:?}", hdr);
+                    println!("{:x?}", buf);
                 } else {
                     println!("error parsing");
                 }
