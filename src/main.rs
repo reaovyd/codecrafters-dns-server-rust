c17e7397c51aa6b0237049c41dc225fbf213887b
reaovyd <zackbellzokobell@gmail.com>
2023-11-29 23:40:09 -0500

    fix: 🐛 push

--- src/main.rs
+++ src/main.rs
@@ -33,67 +33,67 @@ fn main() {
                                 match hdr.qr() {
                                     QrIndicator::Question => {
                                         // TODO: https://www.rfc-editor.org/rfc/rfc1035#section-4.1.4
-                                        let qdcount = hdr.qdcount().to_owned();
                                         println!("{:?}", buf);
-                                        // let ancount = hdr.ancount().to_owned();
-                                        // if let Ok((qsections, _asections)) =
-                                        //     parse_all_sections(payload, qdcount, ancount)
-                                        // {
-                                        //     let mut asections = Vec::new();
-                                        //     let mut ancount = 0;
-                                        //     qsections.iter().for_each(|elem| {
-                                        //         let mut bytes = ASection::new(
-                                        //             elem.section().to_owned(),
-                                        //             (0x000000FF, [0x00, 0x00, 0x00, 0xFF]),
-                                        //             (0x0004, [0x00, 0x04]),
-                                        //             vec![76, 76, 21, 21],
-                                        //         )
-                                        //         .into_bytes();
-                                        //         ancount += 1;
-                                        //         asections.append(&mut bytes);
-                                        //     });
-                                        //     let mut qsections = qsections.into_iter().fold(
-                                        //         Vec::<u8>::new(),
-                                        //         |mut acc: Vec<u8>, elem| {
-                                        //             elem.into_bytes().into_iter().for_each(
-                                        //                 |byte| {
-                                        //                     acc.push(byte);
-                                        //                 },
-                                        //             );
-                                        //             acc
-                                        //         },
-                                        //     );
+                                        let qdcount = hdr.qdcount().to_owned();
+                                        let ancount = hdr.ancount().to_owned();
+                                        if let Ok((qsections, _asections)) =
+                                            parse_all_sections(payload, qdcount, ancount)
+                                        {
+                                            let mut asections = Vec::new();
+                                            let mut ancount = 0;
+                                            qsections.iter().for_each(|elem| {
+                                                let mut bytes = ASection::new(
+                                                    elem.section().to_owned(),
+                                                    (0x000000FF, [0x00, 0x00, 0x00, 0xFF]),
+                                                    (0x0004, [0x00, 0x04]),
+                                                    vec![76, 76, 21, 21],
+                                                )
+                                                .into_bytes();
+                                                ancount += 1;
+                                                asections.append(&mut bytes);
+                                            });
+                                            let mut qsections = qsections.into_iter().fold(
+                                                Vec::<u8>::new(),
+                                                |mut acc: Vec<u8>, elem| {
+                                                    elem.into_bytes().into_iter().for_each(
+                                                        |byte| {
+                                                            acc.push(byte);
+                                                        },
+                                                    );
+                                                    acc
+                                                },
+                                            );
 
-                                        //     let mut res = <[u8; 12]>::from(DnsHeader::new(
-                                        //         hdr.id().to_owned(),
-                                        //         QrIndicator::Reply,
-                                        //         hdr.opcode().to_owned(),
-                                        //         AuthAnswer::NotAuthoritative,
-                                        //         Truncation::NotTruncated,
-                                        //         hdr.recursion_desired().to_owned(),
-                                        //         RecursionStatus::NotAvailable,
-                                        //         0,
-                                        //         match hdr.opcode() {
-                                        //             OpCode::Query => ResponseCode::NoError,
-                                        //             _ => ResponseCode::NotImplemented,
-                                        //         },
-                                        //         *hdr.qdcount(),
-                                        //         ancount,
-                                        //         0,
-                                        //         0,
-                                        //     ))
-                                        //     .to_vec();
+                                            let mut res = <[u8; 12]>::from(DnsHeader::new(
+                                                hdr.id().to_owned(),
+                                                QrIndicator::Reply,
+                                                hdr.opcode().to_owned(),
+                                                AuthAnswer::NotAuthoritative,
+                                                Truncation::NotTruncated,
+                                                hdr.recursion_desired().to_owned(),
+                                                RecursionStatus::NotAvailable,
+                                                0,
+                                                match hdr.opcode() {
+                                                    OpCode::Query => ResponseCode::NoError,
+                                                    _ => ResponseCode::NotImplemented,
+                                                },
+                                                *hdr.qdcount(),
+                                                ancount,
+                                                0,
+                                                0,
+                                            ))
+                                            .to_vec();
 
-                                        //     res.append(&mut qsections);
-                                        //     res.append(&mut asections);
-                                        //     if let Err(err_msg) = udp_socket.send_to(&res, source) {
-                                        //         eprintln!("Error sending message; got {err_msg}");
-                                        //     }
-                                        // } else {
-                                        //     eprintln!(
-                                        //         "Error parsing; not enough bytes for the header!"
-                                        //     );
-                                        // }
+                                            res.append(&mut qsections);
+                                            res.append(&mut asections);
+                                            if let Err(err_msg) = udp_socket.send_to(&res, source) {
+                                                eprintln!("Error sending message; got {err_msg}");
+                                            }
+                                        } else {
+                                            eprintln!(
+                                                "Error parsing; not enough bytes for the header!"
+                                            );
+                                        }
                                     }
                                     QrIndicator::Reply => todo!(),
                                 }
