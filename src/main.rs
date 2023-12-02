9d04e7b6aa8a3bb46a824d5140c350538ac6d2e0
reaovyd <zackbellzokobell@gmail.com>
2023-12-02 16:02:57 -0500

    feat: 🎸 push

--- src/main.rs
+++ src/main.rs
@@ -74,7 +74,8 @@ fn main() {
                             }
                             QueryResponse::Response => match ansection {
                                 Some(ansection) => {
-                                    println!("{:?}", header.txid());
+                                    println!("{:?}, {:?}", header.txid(), transcriber.txid());
+                                    println!("{:?}", ansection);
                                     for group in ansection.groups {
                                         match transcriber.receive_and_delete(header.txid(), group) {
                                             Some((pkt, source)) => {
