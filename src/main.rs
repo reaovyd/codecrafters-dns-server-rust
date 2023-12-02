a533d72f2e34d1e1e5ea103cb8973d1f62124d6e
reaovyd <zackbellzokobell@gmail.com>
2023-12-02 16:32:01 -0500

    feat: 🎸 push

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,6 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((_size, source)) => {
                 let udp_buf = UdpBuffer::new(buf);
-                println!("{:?}", buf);
                 match udp_buf.unpack() {
                     Ok((header, [qsection, ansection, _nssection, _arsection])) => {
                         match header.header_first_half().qr() {
